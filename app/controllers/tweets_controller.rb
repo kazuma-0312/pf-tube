164900831814120b1495aa3f9408fa34e756dbc9
Kazuma Sakamoto <74850915+kazuma-0312@users.noreply.github.com>
2021-02-22 18:39:40 +0900

    コントローラの記述

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,25 @@
 class TweetsController < ApplicationController
   def index
-    
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    url = params[:tweet][:youtube_url]
+    url = url.last(11)
+    @tweet.youtube_url = url
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:title, :youtube_url, :text).merge(user_id: current_user.id)
   end
 end
