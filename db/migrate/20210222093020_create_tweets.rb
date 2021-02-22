46d2f2b64fe9e57f36f18a7e344a29fe6760684d
Kazuma Sakamoto <74850915+kazuma-0312@users.noreply.github.com>
2021-02-22 18:33:11 +0900

    データベースの作成

--- db/migrate/20210222093020_create_tweets.rb
+++ db/migrate/20210222093020_create_tweets.rb
@@ -4,7 +4,7 @@ class CreateTweets < ActiveRecord::Migration[6.1]
       t.string   :title,   null: false
       t.string   :youtube_url, null: false
       t.text   :text,      null: false
-      t.references   :user,         foreign_key: true
+      t.string   :user_id,         foreign_key: true
       t.string   :comment_id,         foreign_key: true
       t.string   :like_id,         foreign_key: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_02_22_093020) do
+
+  create_table "tweets", charset: "utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "youtube_url", null: false
+    t.text "text", null: false
+    t.string "user_id"
+    t.string "comment_id"
+    t.string "like_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
